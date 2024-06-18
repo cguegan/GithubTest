b0714cdee456ccedc5237f98595921eb08785dcf
Christophe <cguegan@gmail.com>
2024-06-18 11:07:22 +0200

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Christophe Guégan on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
