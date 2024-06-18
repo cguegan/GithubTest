cff72c4fc53523f73725e04589ad3448ffc9b27c
Christophe <cguegan@gmail.com>
2024-06-18 12:29:33 +0200

    First modification

--- GithubTest/ContentView.swift
+++ GithubTest/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
             Text("Hello, world!")
+                .font(.largeTitle)
+                .bold()
         }
         .padding()
     }
